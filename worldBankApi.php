6e60a2c81df26d6091f0bb419427810c16df8be9
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-20 17:16:09 -0700

    added methods for country calls, supports all parameters including language and items per page

--- worldBankApi.php
+++ worldBankApi.php
@@ -1,19 +1,57 @@
 <?php
 /** 
- *	The most bare bones API library example
+ *	worldBankApi
  *	Use _request(url,method,data-optional,headers-optional) to access the API
  */
  
 class worldBankApi extends APIBaseClass{
 
-	public static $api_key = '';
 	public static $api_url = 'http://api.worldbank.org/';
 	
 	public function __construct($url=NULL,$api_key=NULL)
 	{
-	// api key for now is optional, but will probably need to look into new_request to ensure the key
-	// is handled properly
 		parent::new_request(($url?$url:self::$api_url));
+	} 
+	
+	public function country_calls_countries($language=NULL,$region=NULL,$incomelevel=NULL,$lendingtype=NULL,$format=NULL){
+		if(func_num_args() > 0) {
+			if($region != NULL) $data ['region'] = $region;
+			if($incomelevel!= NULL) $data['incomelevel'] = $incomelevel;
+			if($lendingtype!= NULL) $data['lendingtype'] = $lendingtype;
+			$data['format'] = ($format != NULL?$format:'json');
+			$result = $this->_request($api_url . ($language !=NULL? $language:NULL) . 'country/' , 'GET',$data);
+		
+		}else
+			$result = $this->_request($api_url . 'country?per_page=10&format=json' );
+	}
+	public function country_calls_regions($language=NULL,$region=NULL,$items_per_page=10,$format=NULL){
+		if(func_num_args() > 0) {
+			if($region != NULL) $data ['region'] = $region;
+			if($items_per_page!= NULL) $data['items_per_page'] = $items_per_page;
+			$data['format'] = ($format != NULL?$format:'json');
+			$result = $this->_request($api_url . ($language !=NULL? $language:NULL) . 'region/' , 'GET',$data);
+					
+		}else
+			$result = $this->_request($api_url . 'region?per_page=10&format=json' );
+		
 	}
 	
+	public function country_calls_lendingtype($language=NULL,$lendingtype=NULL,$items_per_page=10,$format=NULL){
+		if(func_num_args()>0) {
+			if($incomelevel!= NULL) $data['incomelevel'] = $incomelevel;
+			if($lendingtype!= NULL) $data['lendingtype'] = $lendingtype;
+			$result = $this->_request($api_url . ($language !=NULL? $language:NULL) . 'lendingtype' , 'GET',$data);
+		}else
+			$result = $this->_request($api_url .'lendingtype?per_page=10&format=json','GET');
+	
+	}
+	
+	public function country_calls_incomelevel($language=NULL,$incomelevel=NULL,$items_per_page=10,$format=NULL){
+		if(func_num_args()>0) {
+			if($incomelevel!= NULL) $data['incomelevel'] = $incomelevel;
+			if($lendingtype!= NULL) $data['lendingtype'] = $lendingtype;
+			$result = $this->_request($api_url . ($language !=NULL? $language:NULL) . 'lendingtype' , 'GET',$data);
+		}else
+			$result = $this->_request($api_url . 'lendingtype?per_page=10&format=json', 'GET');
+	}
 }
