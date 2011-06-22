36fdf6c5bc3cf1eb07911124b3b44cb755adf7ca
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-22 10:26:11 -0700

    Updated example use with usage, modified method variable order to put language near the end before format

--- example_use.php
+++ example_use.php
@@ -7,5 +7,22 @@ require 'APIBaseClass.php';
 require 'worldBankApi.php';
 
 $new = new worldBankApi();
+
+echo $new->call_topic();
+// reference  http://data.worldbank.org/querybuilder 
+echo $new->call_data('BEL','AG.CON.FERT.ZS');
+
+echo $new->call_source();
+
+echo $new->call_indicator();
+
+echo $new->country_calls_countries();
+
+echo $new->country_calls_regions();
+
+echo $new->country_calls_lendingtype();
+
+echo $new->country_calls_incomelevel();
+
 // Debug information
 die(print_r($new).print_r(get_object_vars($new)).print_r(get_class_methods(get_class($new))));
