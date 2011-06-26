cf07b068eae46b5ba6fa76b2b631e2fd1e3bcfdb
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-25 21:18:36 -0700

    First Commit

--- example_use.php
+++ example_use.php
@@ -2,25 +2,12 @@
 // Base API Class
 require 'APIBaseClass.php';
 
-require 'worldBankApi.php';
+require 'open311Api.php';
 
-$new = new worldBankApi();
+$new = new open311Api();
 
 echo $new->call_topic();
-// reference  http://data.worldbank.org/querybuilder 
-echo $new->call_data('BEL','AG.CON.FERT.ZS');
 
-echo $new->call_source();
-
-echo $new->call_indicator();
-
-echo $new->country_calls_countries();
-
-echo $new->country_calls_regions();
-
-echo $new->country_calls_lendingtype();
-
-echo $new->country_calls_incomelevel();
 
 // Debug information
 die(print_r($new).print_r(get_object_vars($new)).print_r(get_class_methods(get_class($new))));
