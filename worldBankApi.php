98fabb2b55ba593c4d5077e6bf1a46b610c98198
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-07-26 10:01:34 -0700

    changed $api_url to reference class static parameter

--- worldBankApi.php
+++ worldBankApi.php
@@ -21,9 +21,9 @@ class worldBankApi extends APIBaseClass{
 		if(func_num_args() > 0) {
 		// format is kinda silly but here... basically to define as an xml (default) you must set format to XML or anything other than null
 			if($format==NULL) $data['format'] = 'json';
-			$result = $this->_request($api_url . ($language !=NULL? $language . '/':NULL) . "topic" . (is_int($topics)?$topics.'/':NULL)  , 'GET',$data);
+			$result = $this->_request(self::$api_url . ($language !=NULL? $language . '/':NULL) . "topic" . (is_int($topics)?$topics.'/':NULL)  , 'GET',$data);
 		}else{
-			$result = $this->_request($api_url . 'topic?per_page=10&format=json','GET');
+			$result = $this->_request(self::$api_url . 'topic?per_page=10&format=json','GET');
 		}
 		return($result?$result:NULL);
 	
@@ -32,7 +32,7 @@ class worldBankApi extends APIBaseClass{
 		if(is_int($from) && $from < (is_int($to))) $data['date']= $from.$to;
 		$data['format'] = ($format==NULL?'json':$format);
 		$data['per_page'] = $items_per_page;
-		$result = $this->_request($api_url . ($language !=NULL? $language . '/':NULL) . "/countries/$country/indicators/$indicator"  , 'GET',$data);
+		$result = $this->_request(self::$api_url . ($language !=NULL? $language . '/':NULL) . "/countries/$country/indicators/$indicator"  , 'GET',$data);
 		return ($result?$result:NULL);
 	}
 	
@@ -42,12 +42,12 @@ class worldBankApi extends APIBaseClass{
 			if($data_source != NULL && is_integer($data_source)){
 				if($format == NULL) $data['format'] = 'json';
 				$data['per_page'] = $items_per_page;
-				$result = $this->_request($api_url . ($language !=NULL? $language . '/':NULL) . "source/$data_source" , 'GET',$data);
+				$result = $this->_request(self::$api_url . ($language !=NULL? $language . '/':NULL) . "source/$data_source" , 'GET',$data);
 			}
 			if($items_per_page!= NULL) $data['items_per_page'] = $items_per_page;
 				$data['format'] = ($format != NULL?$format:'json');
 		}else
-			$result = $this->_request($api_url . 'source?per_page=10&format=json','GET' );
+			$result = $this->_request(self::$api_url . 'source?per_page=10&format=json','GET' );
 			
 		return ($result?$result:NULL);
 	}
@@ -57,12 +57,12 @@ class worldBankApi extends APIBaseClass{
 			if($indicators != NULL){
 				if($format == NULL) $data['format'] = 'json';
 				$data['per_page'] = $items_per_page;
-				$result = $this->_request($api_url . ($language !=NULL? $language . '/':NULL) . "indicator/$data_source" , 'GET',$data);
+				$result = $this->_request(self::$api_url . ($language !=NULL? $language . '/':NULL) . "indicator/$data_source" , 'GET',$data);
 			}
 			if($items_per_page!= NULL) $data['items_per_page'] = $items_per_page;
 			$data['format'] = ($format != NULL?$format:'json');
 		}else
-			$result = $this->_request($api_url . 'indicator?indicator=10&format=json','GET' );	
+			$result = $this->_request(self::$api_url . 'indicator?indicator=10&format=json','GET' );	
 		return ($result?$result:NULL);
 	}
 	
@@ -72,9 +72,9 @@ class worldBankApi extends APIBaseClass{
 			if($incomelevel!= NULL) $data['incomelevel'] = $incomelevel;
 			if($lendingtype!= NULL) $data['lendingtype'] = $lendingtype;
 			$data['format'] = ($format != NULL?$format:'json');
-			$result = $this->_request($api_url . ($language !=NULL? $language. '/':NULL) . '/country/' , 'GET',$data);
+			$result = $this->_request(self::$api_url . ($language !=NULL? $language. '/':NULL) . '/country/' , 'GET',$data);
 		}else
-			$result = $this->_request($api_url . 'country?per_page=10&format=json','GET' );
+			$result = $this->_request(self::$api_url . 'country?per_page=10&format=json','GET' );
 		return ($result?$result:NULL);	
 	}
 	public function country_calls_regions($region=NULL,$items_per_page=10,$language=NULL,$format=NULL){
@@ -82,10 +82,10 @@ class worldBankApi extends APIBaseClass{
 			if($region != NULL) $data ['region'] = $region;
 			if($items_per_page!= NULL) $data['items_per_page'] = $items_per_page;
 			$data['format'] = ($format != NULL?$format:'json');
-			$result = $this->_request($api_url . ($language !=NULL? $language. '/':NULL) . 'region/' , 'GET',$data);
+			$result = $this->_request(self::$api_url . ($language !=NULL? $language. '/':NULL) . 'region/' , 'GET',$data);
 					
 		}else
-			$result = $this->_request($api_url . 'region?per_page=10&format=json','GET' );
+			$result = $this->_request(self::$api_url . 'region?per_page=10&format=json','GET' );
 		
 		return ($result?$result:NULL);
 	}
@@ -95,9 +95,9 @@ class worldBankApi extends APIBaseClass{
 			if($incomelevel!= NULL) $data['incomelevel'] = $incomelevel;
 			if($lendingtype!= NULL) $data['lendingtype'] = $lendingtype;
 			$data['format'] = ($format != NULL?$format:'json');
-			$result = $this->_request($api_url . ($language !=NULL? $language. '/':NULL) . 'lendingtype' , 'GET',$data);
+			$result = $this->_request(self::$api_url . ($language !=NULL? $language. '/':NULL) . 'lendingtype' , 'GET',$data);
 		}else
-			$result = $this->_request($api_url .'lendingtype?per_page=10&format=json','GET');
+			$result = $this->_request(self::$api_url .'lendingtype?per_page=10&format=json','GET');
 	
 		return ($result?$result:NULL);
 	}
@@ -107,9 +107,9 @@ class worldBankApi extends APIBaseClass{
 			if($incomelevel!= NULL) $data['incomelevel'] = $incomelevel;
 			if($lendingtype!= NULL) $data['lendingtype'] = $lendingtype;
 			$data['format'] = ($format != NULL?$format:'json');
-			$result = $this->_request($api_url . ($language !=NULL? $language. '/':NULL) . 'incomelevel' . ($incomelevel?$incomelevel.'/':NULL) , 'GET',$data);
+			$result = $this->_request(self::$api_url . ($language !=NULL? $language. '/':NULL) . 'incomelevel' . ($incomelevel?$incomelevel.'/':NULL) , 'GET',$data);
 		}else
-			$result = $this->_request($api_url . 'incomelevel?per_page=10&format=json', 'GET');
+			$result = $this->_request(self::$api_url . 'incomelevel?per_page=10&format=json', 'GET');
 	
 		return ($result?$result:NULL);
 	}
